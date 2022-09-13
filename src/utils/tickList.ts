ef5f7ec4ab81ce52843310c6536600628c66db47
samsamlh <46093136+samsamlh@users.noreply.github.com>
2022-09-13 14:16:52 +0200

    Update tickList.ts
    Allow short-ranged tickListDataProvider

--- src/utils/tickList.ts
+++ src/utils/tickList.ts
@@ -26,6 +26,8 @@ export abstract class TickList {
     )
 
     // ensure tick liquidity deltas sum to 0
+    // ?! 
+    /*
     invariant(
       JSBI.equal(
         ticks.reduce((accumulator, { liquidityNet }) => JSBI.add(accumulator, liquidityNet), ZERO),
@@ -33,7 +35,7 @@ export abstract class TickList {
       ),
       'ZERO_NET'
     )
-
+    */
     invariant(isSorted(ticks, tickComparator), 'SORTED')
   }
 
